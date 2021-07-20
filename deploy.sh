2588bf9e4753a3a6d41c46efbaf00c1bf371faf4
Lucas Miranda <lucasalternativo.lg@gmail.com>
2021-07-20 16:52:51 -0300

    Fixed images names

--- deploy.sh
+++ deploy.sh
@@ -1,26 +1,26 @@
 #!/usr/bin/env bash
 
 # build images and tag them
-docker build -t lucasg-mm/multi-client:latest -t lucasg-mm/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t lucasg-mm/multi-server:latest -t lucasg-mm/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t lucasg-mm/multi-worker:latest -t lucasg-mm/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker build -t lucasgmm20/multi-client:latest -t lucasgmm20/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t lucasgmm20/multi-server:latest -t lucasgmm20/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t lucasgmm20/multi-worker:latest -t lucasgmm20/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
 
 # push images (with every tag) to Docker Hub
-docker push lucasg-mm/multi-client:latest
-docker push lucasg-mm/multi-client:$SHA
+docker push lucasgmm20/multi-client:latest
+docker push lucasgmm20/multi-client:$SHA
 
-docker push lucasg-mm/multi-server:latest
-docker push lucasg-mm/multi-server:$SHA
+docker push lucasgmm20/multi-server:latest
+docker push lucasgmm20/multi-server:$SHA
 
-docker push lucasg-mm/multi-worker:latest
-docker push lucasg-mm/multi-worker:$SHA
+docker push lucasgmm20/multi-worker:latest
+docker push lucasgmm20/multi-worker:$SHA
 
 
 # apply kubes' config files
 kubectl apply -f k8s
 
 # updates images used in kubes' deployments
-kubectl set image deployments/client-deployment client=lucasg-mm/multi-client:$SHA
-kubectl set image deployments/server-deployment server=lucasg-mm/multi-server:$SHA
-kubectl set image deployments/worker-deployment worker=lucasg-mm/multi-worker:$SHA
+kubectl set image deployments/client-deployment client=lucasgmm20/multi-client:$SHA
+kubectl set image deployments/server-deployment server=lucasgmm20/multi-server:$SHA
+kubectl set image deployments/worker-deployment worker=lucasgmm20/multi-worker:$SHA
